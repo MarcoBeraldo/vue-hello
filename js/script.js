77bb4432f6d8c96e407568ef70180dddedff3a42
MarcoBeraldo <beraldomar@gmail.com>
2022-07-11 16:41:33 +0200

    completed

---
+++ js/script.js
@@ -0,0 +1,10 @@
+console.log('Vue ok', Vue)
+
+const root = new Vue({
+    el: '#root',
+    data: {
+        text: 'Hello VueJS!',
+        imageUrl: 'https://www.giuseppemaccario.com/it/wp-content/uploads/2019/04/Vue.js.png'
+    }
+
+})
